2cb4d48b19c658663634ed92969cb98ebe482fd0
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 <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>
2024-12-10 09:32:56 +0100

    add login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/users', userController.getUser)
 router.post('/users', userController.createUser)
-
+router.post('/login', userController.login)
 
 
 module.exports = router
